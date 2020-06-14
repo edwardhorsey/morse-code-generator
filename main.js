1c8e382861737400aa47a9267c58c260099934fb
Edward Horsey <edwardhorsey@me.com>
2020-06-14 10:25:07 +0100

    refract

--- main.js
+++ main.js
@@ -1,88 +1,31 @@
-const morseObj = {
+import * as morse from "./morseObj.js"
 
-  A:  '._'	 	,
-  B:	'_...',
-  C:	'_._.',
-  D:	'_..',
-  E: 	'.',
-  F: 	'.._.',
-  G: 	'__.',
-  H: 	'....',
-  I: 	'..',
-  J: 	'.___',
-  K: 	'_._',
-  L: 	'._..',
-  M: 	'__',
-  N: 	'_.',
-  O:  '___',
-  P:  '.__.',
-  Q:  '__._',
-  R:  '._.',
-  S:  '...',
-  T:  '_',
-  U:  '.._',
-  V:  '..._',
-  W:  '.__',
-  X:  '_.._',
-  Y:  '_.__',
-  Z:  '__..',
+const userInput = document.getElementById('input-in');
+const langDetectOutput = document.getElementById('language-detect');
+const outputBox = document.getElementById('morse-output');
+const submitButton = document.getElementById('submit-button');
 
-	1:	'.____',
-	2:	'..___',
-	3:	'...__',
-	4:	'...._',
-	5:	'.....',
-  6:	'_....'	,
-  7:	'__...'	,
-  8:	'___..'	,
-  9:	'____.'	,
-  0:	'_____',
-
-  ',': '__..__',
-  '.':'._._._',
-  '?':'..__..',
-  ';':'_._._.',
-  ':':'___...',
-  '/':'_.._.',
-  '-':'_...._',
-  "'":'.____.',
-  '"':'._.._.',
-  '(':'_.__.',
-  ')':'_.__._',
-  '=':'_..._',
-  '+':'._._.',
-  '*':'_.._',
-  '@':'.__._.',
-
-}
+const morseDetectionHyphen = (string) => /^[\.-\s\|]+$/.test(string);
+const morseDetectionUnderscore = (string) => /^[\._\s\|]+$/.test(string);
 
 const toHyphens = (str) => str.split('').map(e=>e.replace('_', '-')).join('');
-
 const fromHyphens = (str) => str.split('').map(e=>e.replace('-', '_')).join('');
-
 const trimWhiteSpace = (str) => str.replace(/\s+/g, ' ').trim();
 
 const turnIntoMorse = (str) => {
   let output = trimWhiteSpace(str).toUpperCase().split('').map(e=>{
-    return e === ' ' ? ' | ' : morseObj[e];
+    return e === ' ' ? ' | ' : morse.morseObj[e];
   }).join(' ');
   return toHyphens(output);
 };
 
 const turnIntoEnglish = (str) => {
   let output = trimWhiteSpace(str).toUpperCase().split(' ').map(e=>{
-    return e === '|' ? e : Object.keys(morseObj).find(k=>{return morseObj[k] === e});
+    return e === '|' ? e : Object.keys(morse.morseObj).find(k=>{return morse.morseObj[k] === e});
   })
   return output.map(e=>e.replace('|', ' ')).join('');
 }
 
-const morseDetectionHyphen = (string) => /^[\.-\s\|]+$/.test(string);
-const morseDetectionUnderscore = (string) => /^[\._\s\|]+$/.test(string);
-
-const userInput = document.getElementById('input-in');
-const langDetectOutput = document.getElementById('language-detect');
-const outputBox = document.getElementById('morse-output');
-const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', ()=>{
   let input = userInput.value;
